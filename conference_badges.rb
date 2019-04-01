2aca843ef6a0b827a840e6c6ce5b848d14a284be
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-01 20:07:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(names)
+  
+
+end
+
+def assign_rooms
+
+end
+
+def printer
+
+end
