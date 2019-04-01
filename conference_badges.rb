0c44e725c1ff178e8fc424bd57520ca7db23d133
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-01 20:23:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,13 @@
 # Write your code here.
-name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(name)
-  return "Hello, my name is #{name}"
+def batch_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers = [ ]
+
+
 
 end
 
